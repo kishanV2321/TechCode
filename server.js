3095cf38540c61dc285b1b58bc4ea13bd3ff51a5
kishanV2123 <kv230801@gmail.com>
2024-01-31 12:38:57 +0530

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 // Set the view engine to ejs
 app.set('view engine', 'ejs');
